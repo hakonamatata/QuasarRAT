255f7d13acfdee6b49af55ec693f9c313fff243f
Jan Sviland <jan.sviland@focus.no>
2015-10-21 16:28:09 +0200

    Added a downloadDirectory ServerPacket

---
+++ Client/Core/Packets/ServerPackets/DownloadDirectory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using xClient.Core.Networking;
+
+namespace xClient.Core.Packets.ServerPackets
+{
+    [Serializable]
+    class DownloadDirectory : IPacket
+    {
+
+        public string RemotePath { get; set; }
+
+        public DownloadDirectory()
+        {
+        }
+
+        public DownloadDirectory(string remotepath)
+        {
+            this.RemotePath = remotepath;
+        }
+
+        public void Execute(Client client)
+        {
+            client.Send(this);
+        }
+    }
+}
